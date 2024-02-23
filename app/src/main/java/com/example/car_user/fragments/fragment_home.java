96d92ed86367b4359f86eef4894728a58543f97a
dharmika-shakti <dharmika.shaktiwebsolutions@gmail.com>
2024-02-23 13:21:35 +0530

    fragment_home

--- app/src/main/java/com/example/car_user/fragments/fragment_home.java
+++ app/src/main/java/com/example/car_user/fragments/fragment_home.java
@@ -1,4 +1,4 @@
-package com.example.car_user;
+package com.example.car_user.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.car_user.R;
 
 /**
  * A simple {@link Fragment} subclass.
